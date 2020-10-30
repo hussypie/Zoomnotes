30b146e453f5149908ab332cf304499ee4eacb0f
Bertalan Körmendy <korber314@gmail.com>
2020-10-30 16:05:23 +0100

    make file browser node codable

--- Zoomnotes/ViewModels/FileBrowser/FileBrowserViewModel+Node.swift
+++ Zoomnotes/ViewModels/FileBrowser/FileBrowserViewModel+Node.swift
@@ -46,12 +46,12 @@ enum FolderBrowserNodeStoreID: Codable {
     }
 }
 
-class FolderBrowserNode: Codable {
+class FolderBrowserNode: ObservableObject, Codable {
     let id: UUID
     let store: FolderBrowserNodeStoreID
-    var preview: CodableImage
-    var name: String
-    var lastModified: Date
+    @Published var preview: CodableImage
+    @Published var name: String
+    @Published var lastModified: Date
 
     init(id: UUID,
          store: FolderBrowserNodeStoreID,
@@ -64,6 +64,29 @@ class FolderBrowserNode: Codable {
         self.preview = preview
         self.name = name
         self.lastModified = lastModified
+    }
+
+    enum CodingKeys: CodingKey {
+        case id, store, preview, name, lastModified
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.store = try container.decode(FolderBrowserNodeStoreID.self, forKey: .store)
+        self.preview = try container.decode(CodableImage.self, forKey: .preview)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.lastModified = try container.decode(Date.self, forKey: .lastModified)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(store, forKey: .store)
+        try container.encode(preview, forKey: .preview)
+        try container.encode(name, forKey: .name)
+        try container.encode(lastModified, forKey: .lastModified)
     }
 }
 
